95276630eb4b9ecda8f9addc54668c7ecb142797
elyas20 <iliasdahi@gmail.com>
2018-06-22 10:19:58 +0100

    Update AWSIOT_Client.py

--- AWSIOT_Client.py
+++ AWSIOT_Client.py
@@ -29,7 +29,7 @@ class shadowCallbackContainer:
         print("Received a delta message:")
         payloadDict = json.loads(payload)
         deltaMessage = json.dumps(payloadDict["state"])
-  print(deltaMessage)
+  	print(deltaMessage)
         print("Request to update the reported state...")
         newPayload = '{"state":{"reported":' + deltaMessage + '}}'
         self.deviceShadowInstance.shadowUpdate(newPayload, None, 5)
