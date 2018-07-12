9772e9988590d30f7f2ca1180f54b8f7e1317a28
elyas20 <iliasdahi@gmail.com>
2018-07-12 18:21:10 +0100

    Update AWSIOT_Client.py

--- AWSIOT_Client.py
+++ AWSIOT_Client.py
@@ -17,6 +17,13 @@ import json
 import cv2
 from uuid import getnode as get_mac
 import time
+import psycopg2
+
+
+import fnmatch, os 	
+
+
+
 
 class shadowCallbackContainer:
     def __init__(self, deviceShadowInstance):
@@ -29,9 +36,10 @@ class shadowCallbackContainer:
         print("Received a delta message:")
         payloadDict = json.loads(payload)
         deltaMessage = json.dumps(payloadDict["state"])
-  	print(deltaMessage)
+        print(deltaMessage)
         print("Request to update the reported state...")
         newPayload = '{"state":{"reported":' + deltaMessage + '}}'
+
         self.deviceShadowInstance.shadowUpdate(newPayload, None, 5)
         print("Sent.")
 def customShadowCallback_Update(payload, responseStatus, token):
@@ -46,23 +54,23 @@ def customShadowCallback_Update(payload, responseStatus, token):
         print("property: " + str(payloadDict["state"]["desired"]["live"]))
         print("~~~~~~~~~~~~~~~~~~~~~~~\n\n")
     if responseStatus == "rejected":
-    	print("Update request " + token + " rejected!")
+        print("Update request " + token + " rejected!")
 
 def customShadowCallback_Delete(payload, responseStatus, token):
+
+ # payload is a JSON string ready to be parsed using json.loads(...)
+    # in both Py2.x and Py3.x
     if responseStatus == "timeout":
-        print("Delete request " + token + " time out!")
+        print("Update request " + token + " time out!")
     if responseStatus == "accepted":
+        payloadDict = json.loads(payload)
         print("~~~~~~~~~~~~~~~~~~~~~~~")
-        print("Delete request with token: " + token + " accepted!")
+        print("Update request with token: " + token + " accepted!")
+        print("property: " + str(payloadDict["state"]["desired"]["live"]))
         print("~~~~~~~~~~~~~~~~~~~~~~~\n\n")
     if responseStatus == "rejected":
-        print("Delete request " + token + " rejected!")
+        print("Update request " + token + " rejected!")
 
-def customCallback(client, userdata, message):
-
-    print("Received a new message: ")
-    print(message.payload)
-    print("from topic: ")
 def customShadowCallback_Delete(payload, responseStatus, token):
     if responseStatus == "timeout":
         print("Delete request " + token + " time out!")
@@ -80,10 +88,36 @@ def customCallback(client, userdata, message):
     print("from topic: ")
     print(message.topic)
     print("--------------\n\n")
-ShadowClient = AWSIoTMQTTShadowClient("litterbug_shadow")
+
+def get_rootca():
+
+	for file in os.listdir('.certifcations/'):
+	    if fnmatch.fnmatch(file, "*.pem"):
+	    	print (".certifcations/"+file)
+	    	return (".certifcations/"+file)
+def get_cert():
+
+	for file in os.listdir('.certifcations/'):
+	    if fnmatch.fnmatch(file, '*.pem.crt'):
+	    	print (".certifcations/"+file)
+	    	return (".certifcations/"+file)
+def get_private():
+
+	for file in os.listdir('.certifcations/'):
+	    if fnmatch.fnmatch(file, '*.pem.key'):
+	    	print (".certifcations/"+file)
+	    	return (".certifcations/"+file)
+
+
+
+
+
+
+ShadowClient = AWSIoTMQTTShadowClient("ubuntu-desktop")
+
 ShadowClient.configureEndpoint("a1oa9tg9lcso0.iot.eu-west-1.amazonaws.com", 8883)
-ShadowClient.configureCredentials("/home/pi/certs/VeriSign-Class 3-Public-Primary-Certification-Authority-G5.pem", 
-"/home/pi/certs/b426e422b6-private.pem.key", "/home/pi/certs/b426e422b6-certificate.pem.crt")
+ShadowClient.configureCredentials(get_rootca(), 
+get_private(),get_cert())
 ShadowClient.configureAutoReconnectBackoffTime(1, 32, 20)
 ShadowClient.configureConnectDisconnectTimeout(10)  # 10 sec
 ShadowClient.configureMQTTOperationTimeout(5)  # 5 sec
@@ -91,13 +125,16 @@ ShadowClient.connect()
 deviceShadowHandler = ShadowClient.createShadowHandlerWithName("Litterbug", True)
 shadowCallbackContainer_Bot = shadowCallbackContainer(deviceShadowHandler)
 #deviceShadowHandler.shadowRegisterDeltaCallback(shadowCallbackContainer_Bot.customShadowCallback_Delta)
+
+
 mac = get_mac()
 print (mac)
+
 # MQTT Connection establishement
 myMQTTClient = AWSIoTMQTTClient("Litterbug_desktop")
 myMQTTClient.configureEndpoint("a1oa9tg9lcso0.iot.eu-west-1.amazonaws.com", 8883)
-myMQTTClient.configureCredentials("/home/pi/certs/VeriSign-Class 3-Public-Primary-Certification-Authority-G5.pem", 
-"/home/pi/certs/b426e422b6-private.pem.key", "/home/pi/certs/b426e422b6-certificate.pem.crt")
+myMQTTClient.configureCredentials(get_rootca(), 
+get_private(),get_cert())
 
 
 myMQTTClient.configureOfflinePublishQueueing(-1)  # Infinite offline Publish queueing
@@ -106,13 +143,19 @@ myMQTTClient.configureConnectDisconnectTimeout(10)  # 10 sec
 myMQTTClient.configureMQTTOperationTimeout(5)  # 5 sec
 #connect and publish
 myMQTTClient.connect()
-s3 = boto3.client('s3', aws_access_key_id="xxxxxxxxxxx",
-aws_secret_access_key="yyyyyyyyyyyyyyyyyyyyyyy")
+s3 = boto3.client('s3', aws_access_key_id= os.environ["ACCESSKEY"],
+aws_secret_access_key= os.environ["SECRETKEY"])
 
 
-s3.download_file('littercam','device-'+str(mac)+'/parameters.txt', '/home/pi/littering/parameters.txt')
+con = psycopg2.connect("host='litterbugdb.c1ekrfqx70oj.eu-west-1.rds.amazonaws.com' dbname='littering' user='ilias' password='Algeria7201990'")
+cur = con.cursor()
+cur.execute("SELECT id FROM devices where mac_addr='%s'",[mac])
+row = cur.fetchone()
+device_id=row[0]
+print (device_id)
+s3.download_file('littercam','device-'+str(mac)+'/parameters.txt', '.parameters.txt')
 
-subprocess.Popen(["/home/pi/littering/Litter_detect"])
+#subprocess.Popen(["./Litter_detect"])
 myMQTTClient.publish("topic", "connected", 0)
 
 timestr = time.strftime("%Y-%m-%d-%H:%M:%S")
@@ -122,9 +165,10 @@ timestr = time.strftime("%Y-%m-%d-%H:%M:%S")
 #s3.upload_file("/home/pi/detections/litter.jpg", "littercam", "device-"+str(mac)+"/litter"+str(timestr)+".jpg")         
 
 # inotify initialization 
+
 inotify = INotify()
 watch_flags = flags.CREATE | flags.MODIFY 
-wd = inotify.add_watch('/home/pi/littering/detections', watch_flags)
+wd = inotify.add_watch('detections', watch_flags)
 while 1:
        timestr = time.strftime("%Y-%m-%d-%H:%M:%S")
        JSONPayload = '{"state":{"desired":{"live": \"'+timestr+'\" }}}'
@@ -134,23 +178,29 @@ while 1:
        print ("evnets "+ str(events))
        if events: 
            #if litter event send detection image to S3 bucket "littercam"
-            image=cv2.imread("/home/pi/littering/detections/litter.bmp");
-            cv2.imwrite('/home/pi/detections/litter.jpg',image,[int(cv2.IMWRITE_JPEG_QUALITY), 50])
+            image=cv2.imread("detections/litter.bmp");
+            cv2.imwrite('detections/litter.jpg',image,[int(cv2.IMWRITE_JPEG_QUALITY), 50])
             timestr = time.strftime("%Y-%m-%d-%H:%M:%S")
 
-            s3.upload_file("/home/pi/detections/litter.jpg", "littercam", "device-"+str(mac)+"/litter"+str(timestr)+".jpg")
-            s3.upload_file("/home/pi/detections/litter.jpg", "littercam", "device-"+str(mac)+"/litter.jpg")
+            s3.upload_file("detections/litter.jpg", "littercam", "device-"+str(mac)+"/litter"+str(timestr)+".jpg")
+            s3.upload_file("detections/litter.jpg", "littercam", "device-"+str(mac)+"/litter.jpg")
            # and send message to the MQTT topic 
-	    message = {}
+            detection= "device-"+str(mac)+"/litter"+str(timestr)+".jpg"
+            message = {}
             message['message'] = " A littering event is detected"
             message['imageKey'] = events[-1].name
             message['device_id'] = "ID_1" 
-            with open('/home/pi/littering/litterlog.txt', 'a') as the_file:
+            with open('litterlog.txt', 'a') as the_file:
                     the_file.write("A littering is detected at "+timestr+"\n")         
-            s3.upload_file("/home/pi/littering/litterlog.txt", "littercam", "device-"+str(mac)+"/log.txt")
+            s3.upload_file("litterlog.txt", "littercam", "device-"+str(mac)+"/log.txt")
             messageJson = json.dumps(message)
             myMQTTClient.publish("topic", messageJson, 1)
+            #cur.execute("UPDATE events SET detection= %s, confidence = %s WHERE device_id = %s",(detection,95,device_id))
+            stat=cur.execute("INSERT INTO events ( detection, confidence, device_id) VALUES (%s,%s,%s) ",(detection,95,device_id))
+            con.commit()
+	
             event=events[-1] 
+ 
             print(event)
            # for flag in flags.from_mask(event.mask):
             #   print('    ' + str(flag))
